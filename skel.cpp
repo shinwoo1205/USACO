d3c2420c4945b24acdf673aca88fa7b7cba3b835
Jung Shinwoo <jeremiah52@naver.com>
2019-02-07 14:56:34 +0900

    skel

--- skel.cpp
+++ skel.cpp
@@ -1,1 +1,20 @@
+// codeground의 skel code 가져옴
 #include <stdio.h>
+int answer;
+
+int main(void)
+{
+  int T, test_case;
+
+  freopen ("input.txt", "r", stdin);
+
+  cin >> T;
+  for (test_case = 0; test_case < T; test_case ++) {
+    Answer = 0;
+    
+    cout << "Case #" << test_case + 1 << endl;
+    cout << Answer << endl;
+  }
+
+  return 0;
+}
