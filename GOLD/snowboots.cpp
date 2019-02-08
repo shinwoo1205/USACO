55c84d706446efa9b3291d5d0edaaede4ea75bee
Jung Shinwoo <jeremiah52@naver.com>
2019-02-08 09:42:10 +0900

    dd

---
+++ GOLD/snowboots.cpp
@@ -0,0 +1,16 @@
+// modified codeground's skel code
+#include <stdio.h>
+int Answer;
+
+int main(void)
+{
+  int N;
+  freopen ("input.txt", "r", stdin);
+
+  cin >> N;
+  Answer = 0;
+    
+  cout << Answer << endl;
+
+  return 0;
+}
