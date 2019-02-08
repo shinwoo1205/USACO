a061da0b2ed0120910968e4716398c112a05270c
Jung Shinwoo <jeremiah52@naver.com>
2019-02-08 10:27:01 +0900

    snowboot1

--- GOLD/snowboots.cpp
+++ GOLD/snowboots.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
-int Answer;
 //s: at most s feets of snow that FJ can step 
 //d: the maximum distance that FJ can step once
 struct boot {
@@ -14,23 +13,23 @@ vector<boot> boots;
 
 int main(void)
 {
+  ofstream fout ("snowboots.out");
+  ifstream fin ("snowboots.in");
   // N: the number of tiles
   // B: the number of boots
   int N, B;
-  freopen ("input.txt", "r", stdin);
 
-  cin >> N >> B;
+  fin >> N >> B;
   feets.resize(N);
   boots.resize(B);
-  Answer = 0;
   // initialize tiles
   for (int i = 0; i < N; i++)
-    cin >> feets[i]
+    fin >> feets[i]
   // initialize boots
   for (int i = 0; i < B; i++_
-    cin >> boots[i].s >> boots[i].d;
+    fin >> boots[i].s >> boots[i].d;
   
-  cout << Answer << endl;
+  fout << endl;
 
   return 0;
 }
