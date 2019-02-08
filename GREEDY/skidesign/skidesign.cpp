79d9a7b76c3a86318d6f467634c4d79c0015ad4b
Jung Shinwoo <jeremiah52@naver.com>
2019-02-08 18:22:03 +0900

    intermideate

--- GREEDY/skidesign/skidesign.cpp
+++ GREEDY/skidesign/skidesign.cpp
@@ -4,27 +4,45 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-
 vector<int> hills;
-int costs[84];
+vector<int> costs(84);
 
+void calculate(const int min, const int max);
 int main(void)
 {
   ofstream fout ("skidesign.out");
   ifstream fin ("skidesign.in");
   
-  memset(costs, 0, sizeof(int) * 84);
-
   int N;
   fin >> N;
   hills.resize(N);
   for (int i = 0; i < N; i++)
     fin >> hills[i];
-  fout << endl;
-
-
+  for (int i = 0; i < 84; i++)
+    calculate(i, i+17);
+  sort(costs.begin(), costs.end());
+  
+  fout << costs[0] << endl;
 
   return 0;
 }
+void calculate(const int min, const int max)
+{
+  int maxIter = hills.size();
+  int sum = 0;
+  for (int i = 0 ; i < maxIter; i++) {
+    if (hills[i] < min) { 
+      int diff = (min - hills[i]);
+      sum += (diff*diff); 
+    }
+    if (hills[i] > max) {
+      int diff = (hills[i] - max);
+      sum += (diff*diff);
+    }
+  }   
+  costs[min] = sum;
+}
+
