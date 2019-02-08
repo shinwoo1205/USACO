de707e4d4902f27a746c0760f9236ec7f55838f2
Jung Shinwoo <jeremiah52@naver.com>
2019-02-08 15:39:00 +0900

    skidesign

---
+++ GREEDY/skidesign/skidesign.cpp
@@ -0,0 +1,17 @@
+// USACO skel
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(void)
+{
+  ofstream fout ("test.out");
+  ifstream fin ("test.in")
+
+  int N;
+  fin >> N;
+  fout << endl;
+
+  return 0;
+}
