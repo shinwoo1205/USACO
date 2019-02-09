3d14f4a9cc36cd00f5605fa82a690b2d3df830c0
Jung Shinwoo <jeremiah52@naver.com>
2019-02-09 17:30:49 +0900

    add the logic that process possible value of FJkey and masterkey

--- GREEDY/combo/combo.cpp
+++ GREEDY/combo/combo.cpp
@@ -10,23 +10,58 @@ LANG: C++
 #include <vector>
 
 using namespace std;
-
+//Farmer John's key and Master Key
 vector<int> FJkey(3);
-vector<int> master(3);
+vector<int> Mkey(3);
+//Farmer John's possible value and master's possible value
+int FJPossible[3][5];
+int MPossible[3][5];
+int N;
 
+void determinePossible(const int keyvalue, int* possible);
 int main(void)
 {
   ofstream fout ("combo.out");
   ifstream fin ("combo.in");
 
-  int N;
   fin >> N;
   for (int i  = 0; i < 3; i++)
     fin >> FJkey[i];
   for (int i  = 0; i < 3; i++)
-    fin >> master[i];
+    fin >> Mkey[i];
+  for (int i = 0; i < 3; i++){
+    determinePossible(FJkey[i], FJPossible[i]);
+    determinePossible(Mkey[i], MPossible[i]);    
+  }  
+  for (int i = 0; i < 3; i++)
+  {
+    cout << "FJpossible" << "[" << i << "]: " ;
+    for (int j = 0; j < 5; j ++)
+      cout << FJPossible[i][j] << " ";
+    cout << endl;
+  }  
+
   
   fout << endl;
 
   return 0;
+}
+//define possible value  (value-2, value-1, value, value+1, value+2)
+void determinePossible(const int keyvalue, int* possible)
+{
+  possible[0] = keyvalue;
+  ((keyvalue + 1) <= N) ? possible[1] = (keyvalue + 1) : possible[1] = 0;
+  ((keyvalue + 2) <= N) ? possible[2] = (keyvalue + 2) : possible[2] = 0;  
+  ((keyvalue - 1) > 0) ? possible[3] = keyvalue - 1 : possible[3] = N;
+  ((keyvalue - 2) > 0) ? possible[4] = keyvalue - 2 : possible[4] = N - 1;
+
+  // delete duplicate value
+  for(int i = 0; i < 5; i++)
+    for(int j = i + 1; j < 5; j++){
+      if (possible[i] == 0) 
+        break;
+      else
+        if (possible[i] == possible[j])
+          possible[j] = 0;
+    }
 }
