d00bd8d97476efb12db76021ff06422264784c6d
Jung Shinwoo <jeremiah52@naver.com>
2019-02-10 21:55:58 +0900

    process input

--- GREEDY/crypt1/crypt1.cpp
+++ GREEDY/crypt1/crypt1.cpp
@@ -8,13 +8,24 @@ LANG: C++
 #include <fstream>
 #include <string>
 
+using namespace std;
+
+//a bit mask for given digits
+int givendigit = 0;
 int main(void)
 {
   ofstream fout ("crypt1.out");
   ifstream fin ("crypt1.in");
 
+  // process inputs
   int N;
   fin >> N;
+  for (int i = 0; i < N; i++) {
+    int temp;
+    fin >> temp;
+    temp -= 1;
+    givendigit |= (1 << temp);  
+  }
   fout << endl;
 
   return 0;
