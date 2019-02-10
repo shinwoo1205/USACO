d4880d65f205fcd4831822ee8cdd083e971fd9bf
Jung Shinwoo <jeremiah52@naver.com>
2019-02-10 21:27:52 +0900

    cryptarithm

---
+++ GREEDY/crypt1/crypt1.cpp
@@ -0,0 +1,21 @@
+/*
+ID: jeremia11
+TASK: crypt1
+LANG: C++
+*/
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(void)
+{
+  ofstream fout ("crypt1.out");
+  ifstream fin ("crypt1.in");
+
+  int N;
+  fin >> N;
+  fout << endl;
+
+  return 0;
+}
