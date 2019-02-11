902d42d172ae2706b2462291e7bc81800d71069d
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-11 15:56:52 +0900

    Create billboard.cpp

---
+++ Contest/2017December/Bronze/BlockedBillboard/billboard.cpp
@@ -0,0 +1,28 @@
+/*
+ID: jeremia11
+TASK: billboard
+LANG: C++
+*/
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+  ofstream fout ("billboard.out");
+  ifstream fin ("billboard.in");
+
+  // process inputs
+  int N;
+  fin >> N;
+ 
+  fout << << endl;
+
+
+  return 0;
+}
+
