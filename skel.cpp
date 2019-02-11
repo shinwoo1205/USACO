b0e22bbeb8ff7840c16e61fa173ce9a77a1718bf
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-11 13:27:55 +0900

    Update skel.cpp

--- skel.cpp
+++ skel.cpp
@@ -1,20 +1,28 @@
-// codeground의 skel code 가져옴
+/*
+ID: jeremia11
+TASK: crypt1
+LANG: C++
+*/
 #include <stdio.h>
-int answer;
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main(void)
 {
-  int T, test_case;
+  ofstream fout ("crypt1.out");
+  ifstream fin ("crypt1.in");
 
-  freopen ("input.txt", "r", stdin);
+  // process inputs
+  int N;
+  fin >> N;
+ 
+  fout << << endl;
 
-  cin >> T;
-  for (test_case = 0; test_case < T; test_case ++) {
-    Answer = 0;
-    
-    cout << "Case #" << test_case + 1 << endl;
-    cout << Answer << endl;
-  }
 
   return 0;
 }
+
