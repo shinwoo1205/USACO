bc5ac37bb8a1cde0a8f2af679115292539075fd4
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-11 13:32:41 +0900

    Create wormhole.cpp

---
+++ GREEDY/wormhole/wormhole.cpp
@@ -0,0 +1,28 @@
+/*
+ID: jeremia11
+TASK: wormhole
+LANG: C++
+*/
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+  ofstream fout ("wormhole.out");
+  ifstream fin ("wormhole.in");
+
+  // process inputs
+  int N;
+  fin >> N;
+ 
+  fout << << endl;
+
+
+  return 0;
+}
+
