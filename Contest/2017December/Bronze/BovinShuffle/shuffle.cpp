e883031ded14fda58b227f50dda178d5a4745643
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-12 14:02:05 +0900

    Update shuffle.cpp

--- Contest/2017December/Bronze/BovinShuffle/shuffle.cpp
+++ Contest/2017December/Bronze/BovinShuffle/shuffle.cpp
@@ -17,14 +17,14 @@ vector<string> cownames;
 vector<int> s_order;
 //initial order (1,..., N)
 vector<int> i_order;
-shuffle
+void shuffle();
+int N;
 int main(void)
 {
   ofstream fout ("shuffle.out");
   ifstream fin ("shuffle.in");
 
   // process inputs
-  int N;
   fin >> N;
   s_order.resize(N + 1);
   i_order.resize(N + 1);
@@ -34,8 +34,29 @@ int main(void)
 	  fin >> s_order[i];
   for (int i = 1; i <= N; i++) 
 	  fin >> cownames[i];
+  //initialize initial order 
+  for (int i = 1; i <= N; i++)
+	  i_order[i] = i;
+  //shuffile three time
+  shuffle();
+  shuffle();
+  shuffle();
   
-
+  for (int i = 1; i <= N; i++)
+	  for (int j = 1; j <= N; j++)
+		  if (i == i_order[j])
+			  fout << cownames[j] << endl;
   return 0;
+}
+void shuffle()
+{
+	//temp
+	vector<int> t_order(N + 1);
+	for (int i = 1; i <= N; i++)
+		t_order[s_order[i]] = i_order[i];
+	for (int i = 1; i <= N; i++)
+		i_order[i] = t_order[i];
+	
+	return;
 }
 
