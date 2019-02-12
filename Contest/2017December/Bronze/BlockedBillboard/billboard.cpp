653561eca50aadc49d367702d9837d66284a5995
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-12 10:40:51 +0900

    Update billboard.cpp

--- Contest/2017December/Bronze/BlockedBillboard/billboard.cpp
+++ Contest/2017December/Bronze/BlockedBillboard/billboard.cpp
@@ -11,14 +11,26 @@ LANG: C++
 
 using namespace std;
 
+struct pair {
+	int x;
+	int y;
+};
+struct rect {
+	struct pair lowerleft;
+	struct pair upperright;
+};
 int main(void)
 {
   ofstream fout ("billboard.out");
   ifstream fin ("billboard.in");
 
   // process inputs
-  int N;
-  fin >> N;
+  struct rect board1, board2, truck;
+  fin >> board1.lowerleft.x >> board1.lowerleft.y >> board1.upperright.x >> board1.upperleft.y;
+  fin >> board2.lowerleft.x >> board2.lowerleft.y >> board2.upperright.x >> board2.upperleft.y;
+  fin >> truck.lowerleft.x >> truck.lowerleft.y >> truck.upperright.x >> truck.upperleft.y;
+ 
+ 
  
   fout << << endl;
 
