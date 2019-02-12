726bbd87c117ac932dff4a95dd28ae8bdaffbdbc
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-12 10:46:38 +0900

    Create shuffle.cpp

---
+++ Contest/2017December/Bronze/BovinShuffle/shuffle.cpp
@@ -0,0 +1,28 @@
+/*
+ID: jeremia11
+TASK: shuffle
+LANG: C++
+*/
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+  ofstream fout ("shuffle.out");
+  ifstream fin ("shuffle.in");
+
+  // process inputs
+  int N;
+  fin >> N;
+ 
+  fout << << endl;
+
+
+  return 0;
+}
+
