56dd4c6d22895fcaafe72c7de1b178bc68eed509
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-12 13:18:40 +0900

    Update shuffle.cpp

--- Contest/2017December/Bronze/BovinShuffle/shuffle.cpp
+++ Contest/2017December/Bronze/BovinShuffle/shuffle.cpp
@@ -11,6 +11,11 @@ LANG: C++
 
 using namespace std;
 
+
+vector<string> cownames;
+//shuffle order
+vector<int> s_order;
+shuffle
 int main(void)
 {
   ofstream fout ("shuffle.out");
@@ -19,9 +24,14 @@ int main(void)
   // process inputs
   int N;
   fin >> N;
- 
-  fout << << endl;
-
+  s_order.resize(N+1);
+  cownames.resize(N+1);
+  //process input
+  for (int i = 1; i <= N; i++) 
+	  fin >> s_order[i];
+  for (int i = 1; i <= N; i++) 
+	  fin >> cownames[i];
+  
 
   return 0;
 }
