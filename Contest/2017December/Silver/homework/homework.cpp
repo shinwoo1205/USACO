1f1df0827a1bf213edf094a42ed6be3173c5027a
shinwoo1205 <45053622+shinwoo1205@users.noreply.github.com>
2019-02-13 14:30:44 +0900

    pass all test case
    completed!

--- Contest/2017December/Silver/homework/homework.cpp
+++ Contest/2017December/Silver/homework/homework.cpp
@@ -8,7 +8,7 @@ LANG: C++
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int main(void)
@@ -20,9 +20,12 @@ int main(void)
   int N;
   int sum = 0;
   int minIndex = 0;
+  double maxAverage = 0;
+  int kIndex = 0;
   fin >> N;
   vector<int> scores(N);
   vector<int> sorted(N);
+  vector<double> averages(N-2);
   
   for (int i = 0; i < N; i++) {
 	  fin >> scores[i];
@@ -30,11 +33,32 @@ int main(void)
   }
   //sort 
   sort(sorted.begin(), sorted.end());
+
   //calculate sum
   for (int i = 0; i < N; i++)
 	  sum += scores[i];
   
-  fout << endl;
+  for (int k = 0; k < N-2; k++) {
+	  // when minimal value is deleted
+	  if (scores[k] == sorted[minIndex])
+		  minIndex++;
+	  sum -= scores[k];
+	  int temp = sum - sorted[minIndex];
+	  double tempAverage = (double) temp / (N-k-2);
+	  if (tempAverage >= maxAverage) {
+		  maxAverage = tempAverage;
+	  }
+	  averages[k] = tempAverage;
+	  //debug
+	  //cout << "minIndex : " <<minIndex << "tempAverage: " << tempAverage << "maxAverage: " <<maxAverage <<"kIndex: " <<kIndex <<endl;
+  }
+
+  
+  for (int i = 0; i < N-2; i++) {
+	  if (averages[i] == maxAverage)
+		  fout << (i + 1) << endl;
+  }
+  
 
 
   return 0;
